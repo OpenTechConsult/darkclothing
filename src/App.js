b53de148df7d541bea70c17e38bc5e5108e49299
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-06-01 19:34:04 +0000

    Update our menu-items to navigate with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch ,Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/home.component'
+
+const HatsPage = () => {
+    return (
+    <div>
+        <h1>HATS PAGES</h1>
+    </div>)
+}
 
 function App() {
   return (
     <div>
-        <Home />
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/hats' component={HatsPage} />
+        </Switch>
     </div>
   );
 }
